0f9d2d76e2993e2644c3e9655238875325f2d9cd
El Presidente <bobby.thomas.porter@gmail.com>
2012-11-20 17:11:01 -0900

    Testing

--- MergeConflictTest.java
+++ MergeConflictTest.java
@@ -3,14 +3,14 @@ public class MergeConflictTest {
 	public void test(){
 		int j = 0;
 		j=j+1;
+		j=j-1;
+		j=j*101;
+		j=j+1;
+		j=j+2;
 		j=j+1;
 		j=j+1;
 		j=j+1;
-		j=j+1;
-		j=j+1;
-		j=j+1;
-		j=j+1;
-		j=j+1;
+		j=j+2;
 		
 	}
 	
